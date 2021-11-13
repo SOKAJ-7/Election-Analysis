70794165cfe205bff436c150ff8ef9d8e3c4d553
SOKAJ-7 <93050931+SOKAJ-7@users.noreply.github.com>
2021-11-12 19:56:02 -0500

    Add files via upload
    Code that reads out the election_results.csv file

---
+++ PyPoll.py
@@ -0,0 +1,11 @@
+import os
+import csv
+
+file_to_load = os.path.join('Resources','election_results.csv')
+file_to_save = os.path.join("Analysis","election_analysis.txt")
+
+with open(file_to_load,'r') as election_data:
+    file_reader = csv.reader(election_data)
+    
+    for row in file_reader:
+        print(row)
